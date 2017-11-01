07c499b22042f8047b0046ae60613d18d5704cda
rrooij <rderooij685@gmail.com>
2017-11-01 10:15:56 +0100

    Initial commit

---
+++ VRZKazerneInfo/MainWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Gtk;
+using Pango;
+
+public partial class MainWindow : Gtk.Window
+{
+	private Label clockLabel;
+
+	public MainWindow(string title) : base(title)
+	{
+		var label = new Label("Vandaag prachtig weer");
+
+		var vbox = new VBox();
+		base.ModifyBg(StateType.Normal, new Gdk.Color(0, 0, 0));
+		this.clockLabel = new Label(DateTime.Now.ToString("HH:mm:ss"));
+		this.clockLabel.ModifyFg (StateType.Normal, new Gdk.Color(12, 237, 4));
+		label.ModifyFg(StateType.Normal, new Gdk.Color(12, 237, 4));
+		FontDescription fontDescription = new FontDescription();
+		fontDescription.Size = Convert.ToInt32 (74.0 * Pango.Scale.PangoScale);
+		this.clockLabel.ModifyFont (fontDescription);
+		this.clockLabel.Justify = Justification.Fill;
+		vbox.Add (this.clockLabel);
+		var timer = new Timer(delegate (object state) {
+			this.clockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+		}, null, 0, 1000);
+
+		this.clockLabel.Show ();
+
+		vbox.Add(label);
+		label.Show();
+		this.Add(vbox);
+		vbox.Show();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+}
+
---
+++ VRZKazerneInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace VRZKazerneInfo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Application.Init();
+			MainWindow win = new MainWindow("klok");
+			win.Show();
+			Application.Run();
+		}
+	}
+}
