4ada62cd83ffcd68467c8007644bcb4ea4600245
rrooij <rderooij685@gmail.com>
2017-11-03 11:22:07 +0100

    Move clockLabel to own class

---
+++ VRZKazerneInfo/gui/ClockLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Gtk;
+using Pango;
+
+namespace VRZKazerneInfo
+{
+    /// <summary>
+    /// Simple label for showing the time
+    /// </summary>
+    public class ClockLabel : Gtk.Label
+    {
+        private string text;
+
+        public ClockLabel()
+        {
+            this.Text = DateTime.Now.ToString("HH:mm:ss");
+            this.initializeStyle();
+            this.initializeTimer();
+        }
+
+        private void initializeStyle()
+        {
+            this.ModifyFg(StateType.Normal, new Gdk.Color(12, 237, 4));
+            FontDescription fontDescription = new FontDescription();
+            fontDescription.Size = Convert.ToInt32 (74.0 * Pango.Scale.PangoScale);
+            this.ModifyFont (fontDescription);
+            this.Justify = Justification.Fill;
+        }
+
+        private void initializeTimer()
+        {
+            new Timer(delegate (object state) {
+                this.Text = DateTime.Now.ToString("HH:mm:ss");
+            }, null, 0, 1000);
+        }
+    }
+}
+
--- VRZKazerneInfo/gui/MainWindow.cs
+++ VRZKazerneInfo/gui/MainWindow.cs
@@ -3,40 +3,41 @@ using System.Threading;
 using Gtk;
 using Pango;
 
-public partial class MainWindow : Gtk.Window
+namespace VRZKazerneInfo
 {
-	private Label clockLabel;
 
-	public MainWindow(string title) : base(title)
-	{
-		var label = new Label("Vandaag prachtig weer");
+    public partial class MainWindow : Gtk.Window
+    {
+        private Label clockLabel;
 
-		var vbox = new VBox();
-		base.ModifyBg(StateType.Normal, new Gdk.Color(0, 0, 0));
-		this.clockLabel = new Label(DateTime.Now.ToString("HH:mm:ss"));
-		this.clockLabel.ModifyFg (StateType.Normal, new Gdk.Color(12, 237, 4));
-		label.ModifyFg(StateType.Normal, new Gdk.Color(12, 237, 4));
-		FontDescription fontDescription = new FontDescription();
-		fontDescription.Size = Convert.ToInt32 (74.0 * Pango.Scale.PangoScale);
-		this.clockLabel.ModifyFont (fontDescription);
-		this.clockLabel.Justify = Justification.Fill;
-		vbox.Add (this.clockLabel);
-		var timer = new Timer(delegate (object state) {
-			this.clockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
-		}, null, 0, 1000);
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        /// <param name="title">Title.</param>
+        public MainWindow (string title) : base (title)
+        {
+            var label = new Label ("Vandaag prachtig weer");
+            var clockLabel = new ClockLabel ();
 
-		this.clockLabel.Show ();
+            var vbox = new VBox ();
+            base.ModifyBg (StateType.Normal, new Gdk.Color (0, 0, 0));
+		
+            label.ModifyFg (StateType.Normal, new Gdk.Color (12, 237, 4));
+		
+            vbox.Add (clockLabel);
+            clockLabel.Show ();
 
-		vbox.Add(label);
-		label.Show();
-		this.Add(vbox);
-		vbox.Show();
-	}
+            vbox.Add (label);
+            label.Show ();
+            this.Add (vbox);
+            vbox.Show ();
+        }
 
-	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-	{
-		Application.Quit();
-		a.RetVal = true;
-	}
+        protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+        {
+            Application.Quit ();
+            a.RetVal = true;
+        }
+    }
+
 }
-
