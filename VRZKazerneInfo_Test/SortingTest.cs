27c534d05262f22ee846a555e14bd6a6dbac66ea
rrooij <rderooij685@gmail.com>
2017-11-07 11:04:44 +0100

    Fix tests

--- VRZKazerneInfo_Test/SortingTest.cs
+++ VRZKazerneInfo_Test/SortingTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using VRZKazerneInfo;
 using NUnit.Framework;
 
 namespace VRZKazerneInfo_Test
@@ -6,9 +8,24 @@ namespace VRZKazerneInfo_Test
     [TestFixture]
     public class SortingTest
     {
+        [Test]
         public void integerSortTest()
         {
-            Assert.IsFalse (false); //TODO: Make test
+            List<int> testList = new List<int> ();
+            List<int> testList2 = new List<int> ();
+            testList.Add (1);
+            testList.Add (0);
+            testList.Add (10);
+            testList.Add (8);
+            testList2.Add (0);
+            testList2.Add (1);
+            testList2.Add (8);
+            testList2.Add (10);
+            Sorting.bubbleSort (testList);
+            for(int i = 0; i < testList.Count; i++) 
+            {
+                Assert.AreEqual (testList [i], testList2 [i]);
+            }
         }
     }
 }
